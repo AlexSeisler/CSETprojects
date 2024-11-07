551e23e3a8bec993c0cc5f6b47fe9e3cbb62545e
Alex Seisler <aseisler7045@stevenscollege.edu>
2024-11-06 21:00:46 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function revealProjects() {
+    const reveal = document.querySelector(".reveal");
+  
+    const windowHeight = window.innerHeight;
+    const revealTop = reveal.getBoundingClientRect().top;
+    const revealPoint = 150;
+  
+    if (revealTop < windowHeight - revealPoint) {
+      reveal.classList.add("active");
+    }
+  }
+  revealProjects();
+  reavealProjects();
